e8098b28c882ac73a57d1c1bde5dc0830b0e214e
ziontee113 <ziontee113@gmail.com>
2023-02-21 20:59:37 +0700

    Fetch data from supabase and display them

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["cdni.pornpics.de"],
+    domains: ["i.pinimg.com"],
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,55 @@
 import Image from "next/image";
 import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+type Image = {
+  id: number;
+  href: string;
+  imageSrc: string;
+  name: string;
+  username: string;
+};
+
+export async function getStaticProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+  );
+
+  const { data } = await supabaseAdmin.from("images").select("*").order("id");
+
+  return {
+    props: {
+      images: data,
+    },
+  };
+}
 
 function cn(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Gallery() {
+export default function Gallery({ images }: { images: Image[] }) {
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <BlurImage />
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image}></BlurImage>
+        ))}
       </div>
     </div>
   );
 }
 
-function BlurImage() {
+function BlurImage({ image }: { image: Image }) {
   const [isLoading, setLoading] = useState(true);
 
   return (
-    <a href="#" className="group">
+    <a href={image.imageSrc} className="group">
       <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
         <Image
           alt=""
-          src="https://cdni.pornpics.de/1280/7/56/35353274/35353274_048_8167.jpg"
+          src={image.imageSrc}
           fill
           style={{ objectFit: "cover" }}
           className={cn(
@@ -35,8 +61,8 @@ function BlurImage() {
           onLoadingComplete={() => setLoading(false)}
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">Rock Lee</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@rocklee</p>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
     </a>
   );
 }
